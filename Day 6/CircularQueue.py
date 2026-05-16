2a9ea7b43eae420c39fcbed9b8fe6c0851832f2a
w3nch <133785769+w3nch@users.noreply.github.com>
2026-05-16 16:42:14 +0530

    leared circular queue

---
+++ Day 6/CircularQueue.py
@@ -0,0 +1,11 @@
+class Circular:
+    def __init__(self):
+        self.item = []
+
+    def isEmpty(self):
+        return len(self.item) == 0
+
+    def inque(self):
+        pass
+    def deque(self):
+        pass
